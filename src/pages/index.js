d7d1d938bb12c7d2fee52996e2f0bf02e2b69863
faishal005 <faishalsilentis@gmail.com>
2024-06-25 17:34:05 +0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import Home from "../pages/HomePage/HomePage";
+import AddNote from "../pages/AddNotePage/AddNotePage";
+import EditNote from  "../pages/EditNotePage/EditNotePage";
+import Notes from "../pages/NotesPage/NotesPage";
+import SingleNotePage from "./SingleNotePage/SingleNotePage";
+
+export { Home, AddNote, EditNote, Notes, SingleNotePage};
